fc560502ac66ec6004780530715c45f3b909e964
EdSystango <nitingour99@gmail.com>
2020-02-20 14:12:00 +0530

    delete, mail api

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const mysql=require('mysql')
 const session=require('express-session')
 
 
+
+
+
 //express app creation
 var app=express()
 //configure view engine as hbs
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','hbs')
+
+// caching disabled for every route
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-state=0, post-check=0, pre-check=0');
+  next();
+});
+
+
+
+
 
 app.engine( 'hbs', hbs( {
   extname: 'hbs',
@@ -64,6 +77,8 @@ response.render('login',{msg:'Login Fail,try again'})
 
 
 app.get('/newempform',(req,res)=>{
+  if(req.session.user==null)
+  res.render('login')
 res.render('newemp',{uid:req.session.user})
   })
 
@@ -86,6 +101,8 @@ res.render('newemp',{msg:'Data inserted'})
 })
 
 app.get('/viewemps',(req,res)=>{
+if(req.session.user==null)
+res.render('login')
 //  var loginuser=req.session.user;
 var sql="select * from employee";
 con.query(sql,(err,result)=>{
@@ -100,6 +117,91 @@ req.session.destroy();
 res.render('login',{msg:'Logout successfully'})
 
 })
+
+
+app.get('/deleteEmp',(req,res)=>{
+var empid=req.query.eid;
+console.log(empid);
+var sql="delete from employee where eid=?"
+value=[empid]
+sql=mysql.format(sql,value)
+con.query(sql,(err,result)=>{
+if(err) throw err;
+else if(result.affectedRows!=0)
+{
+  var sql='select * from employee';
+  con.query(sql,(err,result)=>{
+  if(err) throw err;
+  else
+    res.render('showEmp',{data:result,uid:req.session.user,msg:'Data Deleted'})
+  })
+}
+})
+
+})
+
+app.get('/forgetpwd',(req,res)=>{
+  res.render('forgetpwd')
+})
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '$$$$$$$$$$$',
+    pass: '##########'
+  }
+});
+
+
+
+app.post('/getPwd',(req,res)=>{
+  var email=req.body.email;
+var sql='select password from login where uid=?'
+var val=[email]
+sql=mysql.format(sql,val)
+con.query(sql,(err,result)=>{
+if(err)
+throw err;
+else {
+   console.log(result[0].password);
+   var mailOptions = {
+     from: 'demoapitesing@gmail.com',
+     to: email,
+     subject: 'Password Recover for EMS',
+     text: 'Hello '+email+" , your passwword is "+result[0].password
+   };
+   transporter.sendMail(mailOptions, function(error, info){
+     if (error) {
+       console.log(error);
+     } else {
+res.render('forgetpwd',{msg:'Password is sent on your mail id'})
+      // console.log('Email sent: ' + info.response);
+     }
+   });
+
+
+
+
+
+}
+})
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
